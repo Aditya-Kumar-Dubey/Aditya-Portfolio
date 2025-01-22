427c11521cab7d7ae5269ce3c2a0468b17f64d7a
Aditya-Dubey <adityadubey20002001@gmail.com>
2025-01-22 23:41:18 +0530

    add contact component

--- src/app/features/Landing/landing-page/landing-page.component.ts
+++ src/app/features/Landing/landing-page/landing-page.component.ts
@@ -6,12 +6,22 @@ import { AboutComponent } from "../../about-us/about/about.component";
 import { SkillsComponent } from "../../skills/skills/skills.component";
 import { QualificationComponent } from "../../qualification/qualification/qualification.component";
 import { ProjectsComponent } from "../../projects/projects.component";
+import { ContactComponent } from "../../contact/contact.component";
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterModule, FontAwesomeModule, AboutComponent, SkillsComponent, QualificationComponent, ProjectsComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule,
+    AboutComponent,
+    SkillsComponent,
+    QualificationComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
